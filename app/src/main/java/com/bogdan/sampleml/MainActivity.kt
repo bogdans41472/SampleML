ea6654e93bd909dbb452e7706abe8c1be1b9a105
Bogdan Sirbu <mihaibogdans@gmail.com>
2019-08-24 17:53:57 +0100

    Some more cleanup and permission request

--- app/src/main/java/com/bogdan/sampleml/MainActivity.kt
+++ app/src/main/java/com/bogdan/sampleml/MainActivity.kt
@@ -2,26 +2,38 @@ package com.bogdan.sampleml
 
 import android.Manifest.permission.CAMERA
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.wonderkiln.camerakit.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val PERMISSION_REQUEST_CODE: Int = 1
+    private val REQUEST_IMAGE_CAPTURE: Int = 1
+    private val permissionRequestCode: Int = 101
     private val modelPath: String = "mobilenet_quant_v1_224.tflite"
     private val labelPath = "labels.txt"
-    private var classifier: Classifier? = null
     private val inputSize = 224
+
+    private var classifier: Classifier? = null
+    private var currentPhotoPath: String? = null
     private var cameraView: CameraView? = null
     private var executor: Executor = Executors.newSingleThreadExecutor()
 
@@ -53,7 +65,9 @@ class MainActivity : AppCompatActivity() {
         })
 
         button_toggleCamera.setOnClickListener {
-            cameraView?.toggleFacing()
+            if(checkPermission()) {
+                cameraView?.toggleFacing()
+            } else requestPermission()
         }
         button_toggleObjectDetection.setOnClickListener{
             cameraView?.captureImage()
@@ -93,6 +107,52 @@ class MainActivity : AppCompatActivity() {
 
     private fun requestPermission() {
         ActivityCompat.requestPermissions(this,
-            arrayOf(READ_EXTERNAL_STORAGE, CAMERA), PERMISSION_REQUEST_CODE)
+            arrayOf(READ_EXTERNAL_STORAGE, CAMERA), permissionRequestCode)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+            when (requestCode) {
+            permissionRequestCode -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture()
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> {
+                //Do nothing
+            }
+        }
+    }
+
+    private fun takePicture() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val file = createFile()
+        val uri = FileProvider.getUriForFile(
+            this,
+            "com.bogdan.sampleml",
+            file
+        )
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun createFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}",
+            ".jpg",
+            storageDir
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
     }
 }
